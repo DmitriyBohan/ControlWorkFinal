c0a25e4836198ade63fd6eccb613a79935b76ae0
DmitryBohan <zorrorul@gmail.com>
2024-02-26 14:33:18 +0300

    created the ANIMAL class , filled in the Commands interface

--- ControlWorkFinal/JAVA/animals/Animal.java
+++ ControlWorkFinal/JAVA/animals/Animal.java
@@ -1,4 +1,48 @@
 package ControlWorkFinal.JAVA.animals;
 
-public class Animal {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal implements Commands {
+
+    protected String name;
+    protected String type;
+    protected String birthDate;
+    protected List<String> commands;
+
+    public Animal(String name, String type, String birthDate) {
+        this.name = name;
+        this.type = type;
+        this.birthDate = birthDate;
+        this.commands = new ArrayList<>();
+    }
+
+    public Animal() {
+
+    }
+
+    @Override
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    @Override
+    public void addCommand(String command) {
+        commands.add(command);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
 }
--- ControlWorkFinal/JAVA/animals/Commands.java
+++ ControlWorkFinal/JAVA/animals/Commands.java
@@ -1,4 +1,8 @@
 package ControlWorkFinal.JAVA.animals;
 
+import java.util.List;
+
 public interface Commands {
+    List<String> getCommands();
+    void addCommand(String command);
 }
