560ea1bdf7c4b6b18ae38136c3f9be50b2a57dc4
DmitryBohan <zorrorul@gmail.com>
2024-02-26 15:02:20 +0300

    created an entry point

--- ControlWorkFinal/JAVA/AnimalRegistryApp.java
+++ ControlWorkFinal/JAVA/AnimalRegistryApp.java
@@ -1,4 +1,8 @@
 package ControlWorkFinal.JAVA;
 
 public class AnimalRegistryApp {
+    public static void main(String[] args) {
+        AnimalRegistryController controller = new AnimalRegistryController();
+        controller.start();
+    }
 }
